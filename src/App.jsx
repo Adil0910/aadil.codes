8dc271250f0c55e2372a2184523217daefb4f223
Adil0910 <aadilehtesham63@gmail.com>
2026-07-30 13:26:24 +0530

    delete some file

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './component/Navbar';
-import Qualification from './component/Qualification';
 import Home from './component/Home';
-import About from './component/About';
-import Project from './component/Project';
-import Contact from './component/Contact';
+
 function App() {
 return  (
 <BrowserRouter>
 <Navbar/>
 <Routes>
   <Route path='/' element={<Home/>}/>
-  <Route path='/about' element={<About/>}/>
-  <Route path='/project' element={<Project/>}/>
-  <Route path='/qualification' element={<Qualification/>}/>
-  <Route path='/contact' element={<Contact/>}/>
 
 </Routes>
 </BrowserRouter>
